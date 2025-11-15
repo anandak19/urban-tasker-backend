657ca049cf8e6785896d702ee9c5f2ae364765e5
Albin Aji <albinpariyarathu@gmail.com>
2025-11-15 14:32:37 +0530

    Fix: correct errors found in the project

--- src/modules/auth/strategies/google.strategy.ts
+++ src/modules/auth/strategies/google.strategy.ts
@@ -1,28 +1,25 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Profile, Strategy } from 'passport-google-oauth20';
-import { UuidService } from '@core/lib/uuid/uuid.service';
 import { ConfigService } from '@nestjs/config';
 import { AUTH_TOKENS } from '../auth-tokens';
 import { type IAuthService } from '../interfaces/services.interface';
 import { IGoogleUserAuthData } from '../interfaces/auth.interface';
 import { AppConfig } from '@config/app.config';
-// import { IGoogleUserAuthData } from '../interfaces/auth.interface';
+import { AuthService } from '../services/auth/auth.service';
 
 // ERROR: I can't inject auth service from the di container
 // scope is having an effect on this too
 // circular dependecy is initial thougt for error
 // when i inject service normally with out the injection token its working. (google login screen pops up)
 // fix this
+
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  private uuid: UuidService;
-
   constructor(
+    private readonly _authService: AuthService, // Direct injection instead of using injection token
     private readonly configService: ConfigService<AppConfig>,
-    @Inject(AUTH_TOKENS.AUTH_SERVICE) private _authService: IAuthService,
   ) {
-    console.log('Inside GoogleStrategy constructor');
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID', { infer: true })!,
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET', {
@@ -38,8 +35,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     _refreshToken: string,
     profile: Profile,
   ) {
-    console.log('Profile', profile);
-
     // check if the required values are there
     if (
       !profile._json.email ||
@@ -58,7 +53,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 
     // validate user / create user
     const user = await this._authService.validateGoogleAuthUser(userDetails);
-    console.log('Google user in validate', user);
     return user || null;
   }
 }
